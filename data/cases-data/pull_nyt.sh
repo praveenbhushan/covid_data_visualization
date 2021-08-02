f6d07fc381d2a79ad3501c78fec2a5dc6ad9241e
praveenbhushan <40749743+praveenbhushan@users.noreply.github.com>
2021-08-02 00:08:43 -0400

    Header changes

--- data/cases-data/pull_nyt.sh
+++ data/cases-data/pull_nyt.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-cd /Users/sejaldua/Desktop/Spring2020/COMP177/covid-movement-vis/data/cases-data
+#cd /Users/sejaldua/Desktop/Spring2020/COMP177/covid-movement-vis/data/cases-data
+cd /c/PKB/Illinois/Final_Project/covid_test/data/cases-data
 git pull
-cd ../../../covid-19-data
+cd ../..
 git pull
-cp us-counties.csv ../covid-movement-vis/data/cases-data/
-cd ../covid-movement-vis/data/cases-data
+cp us-counties.csv covid_test/data/cases-data/
+cd ./data/cases-data
 python csv2json.py us-counties.csv old-us-counties-cases.json
 python gen_cases_data.py
 mv us-county-cases.json ../../
@@ -14,6 +15,6 @@ echo "Tail: "
 tail -c 56 us-county-cases.json
 echo "\n"
 now="$(date -j -v-1d +'%m/%d')"
-git add data/cases-data/old-us-counties-cases.json data/cases-data/us-counties.csv donut/test.csv us-county-cases.json
-git commit -m "pull $now data"
-git push origin master
+#git add data/cases-data/old-us-counties-cases.json data/cases-data/us-counties.csv donut/test.csv us-county-cases.json
+#git commit -m "pull $now data"
+#git push origin master
